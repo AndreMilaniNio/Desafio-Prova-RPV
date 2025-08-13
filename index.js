984d07beb9b43345c4bee278d177981f83189c27
André Milani <andremilani32@gmail.com>
2025-08-13 12:16:01 -0300

    Terceiro commit - Melhorando a legibilidade do sistema de votação

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const { default: chalk } = require('chalk');
 const prompt = require('prompt-sync')();
 
+
 (function main(){
-    let n = Number(prompt('Quantas vezes votar? '));
+    let n = Number(prompt(chalk.yellow('Quantas vezes votar? ')));
     let candA = 0;
     let candB = 0;
     let candC = 0;
 
-    if (isNaN(n))
+    if (n != Number(n))
         return console.log("Número inválido!");
 
-    console.log(
+    console.log(chalk.green(
         "\n",
         "\nOpções de votação disponíveis:",
         "\nCanditado A = 1",
@@ -17,28 +19,28 @@ const prompt = require('prompt-sync')();
         "\nCanditado C = 3",
         
         "\nSair da votação = 4\n"
-    );
+    ));
 
     for(i = 0; i < n; i++){
 
-        let voto = Number(prompt("Em qual candidato gostaria de votar? "));
+        let voto = Number(prompt(chalk.yellow("Em qual candidato gostaria de votar? ")));
 
         switch(voto){
             case 1:
-                console.log("Voto computado ao candidato A\n")
+                console.log(chalk.red("Voto computado ao candidato A\n"))
                 candA ++;
             break;
             case 2:
-                console.log("Voto computado ao candidato B\n")
+                console.log(chalk.red("Voto computado ao candidato B\n"))
                 candB ++;
             break;
             case 3:
-                console.log("Voto computado ao candidato C\n")
+                console.log(chalk.red("Voto computado ao candidato C\n"))
                 candC ++;
             break;
             default: 
                 n++;
-                console.log("Voto inválido!\nTente novamente!");
+                console.log(chalk.red("Voto inválido!\nTente novamente!"));
             break
         }
     }
