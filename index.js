ddb2b4c5a1dfef3a1d22b5bb2c70922cadb3b258
André Milani <andremilani32@gmail.com>
2025-08-13 12:09:22 -0300

    Segundo commit - melhora no sistema de votação!

--- index.js
+++ index.js
@@ -6,33 +6,41 @@ const prompt = require('prompt-sync')();
     let candB = 0;
     let candC = 0;
 
+    if (isNaN(n))
+        return console.log("Número inválido!");
+
+    console.log(
+        "\n",
+        "\nOpções de votação disponíveis:",
+        "\nCanditado A = 1",
+        "\nCanditado B = 2",
+        "\nCanditado C = 3",
+        
+        "\nSair da votação = 4\n"
+    );
+
     for(i = 0; i < n; i++){
-        console.log(
-            "\n",
-            "\nCanditado A = 1",
-            "\nCanditado B = 2",
-            "\nCanditado C = 3",
-            
-            "\nSair da votação = 4\n"
-        );
 
         let voto = Number(prompt("Em qual candidato gostaria de votar? "));
 
         switch(voto){
             case 1:
-                console.log("Voto computado ao candidato A")
+                console.log("Voto computado ao candidato A\n")
                 candA ++;
             break;
             case 2:
-                console.log("Voto computado ao candidato B")
+                console.log("Voto computado ao candidato B\n")
                 candB ++;
             break;
             case 3:
-                console.log("Voto computado ao candidato C")
+                console.log("Voto computado ao candidato C\n")
                 candC ++;
             break;
+            default: 
+                n++;
+                console.log("Voto inválido!\nTente novamente!");
+            break
         }
     }
-
     console.log(candA, candB, candC);
 })();
